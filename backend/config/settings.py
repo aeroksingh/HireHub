b6047933f447fc5f381170929538addd7ae721b9
aeroksingh <assign131@gmail.com>
2026-01-25 19:11:44 +0530

    added the postgresql db

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -86,12 +86,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 # --------------------------------------------------
 # DATABASE (PostgreSQL on Render, SQLite locally)
 # --------------------------------------------------
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+#         conn_max_age=600,
+#     )
+# }
+
+import dj_database_url
+from decouple import config
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+    'default': dj_database_url.parse(
+        config('DATABASE_URL')
     )
 }
+
 
 
 # --------------------------------------------------
